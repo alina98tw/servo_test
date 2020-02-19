ced9a17f1f219a00ec39991bf2c674a9d50e62de
alina98tw <alina98tw@gmail.com>
2020-02-19 11:35:29 +0100

    movimiento de dos servos FUNCIONA

--- src/servo_test_pack/scripts/two_servo_listener.py
+++ src/servo_test_pack/scripts/two_servo_listener.py
@@ -2,18 +2,23 @@
 
 import rospy, time
 import RPi.GPIO as GPIO
-from servo_test_pack import two_servos
+from servo_test_pack.msg import two_servos
 
-servo = 17
+servo1 = 17
+servo2 = 18
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(servo, GPIO.OUT)
-p = GPIO.PWM(servo, 50)  #GPIO 17 for PWM with 50Hz
-p.start(0)  #Initialization
+GPIO.setup(servo1, GPIO.OUT)
+GPIO.setup(servo2, GPIO.OUT)
+p1 = GPIO.PWM(servo1, 50)  #GPIO 17 for PWM with 50Hz
+p2 = GPIO.PWM(servo2, 50)  #GPIO 18 for PWM with 50Hz
+p1.start(0)  #Initialization
+p2.start(0)
 
 def servo_callback(msg):
-  rospy.loginfo("I heard %s", msg.servo_1)
-  rospy.loginfo("Heard %s", msg.servo_2)
-  p.ChangeDutyCycle(msg.data)
+  rospy.loginfo("Servo 1 to: %s", msg.servo_1)
+  rospy.loginfo("Servo 2 to:  %s", msg.servo_2)
+  p1.ChangeDutyCycle(msg.servo_1)
+  p2.ChangeDutyCycle(msg.servo_2)
 
 def servo_listener():
   rospy.init_node('servo_listener', anonymous=True)
@@ -24,6 +29,7 @@ if __name__=='__main__':
   try:
     servo_listener()
   except rospy.RosInterruptException:
-    p.stop()
+    p1.stop()
+    p2.stop()
     GPIO.cleanup()
     pass
--- src/servo_test_pack/scripts/two_servo_talker.py
+++ src/servo_test_pack/scripts/two_servo_talker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy, time
-from servo_test_pack import two_servos
+from servo_test_pack.msg import two_servos
 import RPi.GPIO as GPIO
 
 def servo_position():
@@ -9,17 +9,23 @@ def servo_position():
    rospy.init_node('servo_talker', anonymous=True)
    rate =  rospy.Rate(5)
    while not rospy.is_shutdown():
-     rospy.loginfo("move servo 1 to position 0: 1.8")
+     rospy.loginfo("move servos to position 0")
      msg_send=two_servos()
      msg_send.servo_1=1.8
      msg_send.servo_2=2.5
      pub.publish(msg_send)
      time.sleep(5)
-     rospy.loginfo("move to position 90: 5.3")
-     pub.publish(5.3)
+     rospy.loginfo("move servos to position 90")
+     msg_send=two_servos()
+     msg_send.servo_1=5.3
+     msg_send.servo_2=6
+     pub.publish(msg_send)
      time.sleep(5)
-     rospy.loginfo("move to position 180: 8.5")
-     pub.publish(8.5)
+     rospy.loginfo("move servos to position 180")
+     msg_send=two_servos()
+     msg_send.servo_1=8.5
+     msg_send.servo_2=10.5
+     pub.publish(msg_send)
      time.sleep(10)
 
 if __name__=='__main__':
